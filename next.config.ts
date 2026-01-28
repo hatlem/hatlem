9baf846aeb7c97efa599fd072c196d1c0f650cdc
hatlem <andreas.hatlem@gmail.com>
2026-01-28 20:40:54 +0100

    fixes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
+import { resolve } from "path";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: resolve(import.meta.dirname),
+  },
 };
 
 export default withNextIntl(nextConfig);
